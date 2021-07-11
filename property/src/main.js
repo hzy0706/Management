dab51ec3d2a57d60cd36d7e4c15c80a26b9bcd72
hzy0706 <85111595+hzy0706@users.noreply.github.com>
2021-07-11 14:37:45 +0800

    修改2

--- property/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
-import 'element-plus/lib/theme-chalk/display.css'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-createApp(App).use(ElementPlus).use(VueAxios, axios).mount('#app')
