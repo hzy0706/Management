0f20bfe379d8a5b34fda79b7e6269b11c1e32235
DESKTOP-A6EN4US\chen <zj1001998@aliyun.com>
2021-07-13 07:48:34 +0800

    之前未提交的

---
+++ property/src/components/charge/billDetails.vue
@@ -0,0 +1,73 @@
+<template>
+
+<el-row :gutter="20">
+  <el-col :span="6"><div ></div></el-col>
+  <el-col :span="6"><div >
+      <h1>账单详情</h1>
+      </div></el-col>
+  <el-col :span="6"><div ></div></el-col>
+
+</el-row>
+<el-row :gutter="15">
+  <el-col :span="3"><div></div></el-col>
+
+   
+  <el-col :span="12"><div class="grid-content bg-purple">
+      <el-table
+    :data="tableData"
+    style="width: 100%"
+    :default-sort="{ prop: 'date', order: 'descending' }"
+    @row-click="goDetails"
+  >
+    <el-table-column prop="date" label="日期" sortable width="180">
+    </el-table-column>
+    <el-table-column prop="name" label="姓名" sortable width="180">
+    </el-table-column>
+    <el-table-column prop="address" label="地址" :formatter="formatter">
+    </el-table-column>
+  </el-table>
+      </div></el-col>
+  <el-col :span="3"><div></div></el-col>
+
+</el-row>
+ 
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
+  },
+  methods: {
+    formatter(row, column) {
+      return row.address;
+    },
+    goDetails(row, column, event) {
+      this.$router.push({ path: "/details/detailsUpdate" }); //id
+    },
+  },
+};
+</script>
--- property/src/components/charge/standingBook.vue
+++ property/src/components/charge/standingBook.vue
@@ -1,39 +1,67 @@
 <!--  -->
 <template>
-  <el-row :gutter="18">
-    <el-col :span="2"
+  <el-row :gutter="20">
+    <el-col :span="3"
       ><div class="grid-content bg-purple">
         <el-tree
           :data="data"
           :props="defaultProps"
           @node-click="handleNodeClick"
+          :node-key="cid"
         ></el-tree></div
     ></el-col>
-    <el-col :span="12" :offset="3"
-      ><div class="grid-content bg-purple">
-        <el-form :inline="true" :model="formInline" class="demo-form-inline">
-          <el-form-item label="审批人">
-            <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+
+    <el-col :span="15"
+      ><div>
+        <el-form :inline="true" :model="formInline" class="demo-form-inline" label-position="right">
+          <el-form-item label="楼盘名称">
+            <el-input v-model="formInline.user" placeholder="楼盘名称"  :readonly="true"></el-input>
           </el-form-item>
-          <el-form-item label="活动区域">
+          <el-form-item label="楼宇">
             <el-select v-model="formInline.region" placeholder="活动区域">
               <el-option label="区域一" value="shanghai"></el-option>
               <el-option label="区域二" value="beijing"></el-option>
             </el-select>
           </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="onSubmit">查询</el-button>
+          <el-form-item label="单元/楼层">
+            <el-select v-model="formInline.region" placeholder="活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
           </el-form-item>
+          <el-form-item label="房间">
+            <el-select v-model="formInline.region" placeholder="活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="费用起期">
+            <el-date-picker v-model="value1" type="date" placeholder="选择日期">
+            </el-date-picker>
+          </el-form-item>
+           <el-form-item label="费用止期">
+            <el-date-picker v-model="value1" type="date" placeholder="选择日期">
+            </el-date-picker>
+          </el-form-item>
+        
         </el-form>
+        <el-row type="flex" >
+
+  <el-col :span="6"><div class="grid-content bg-purple-light"> 台账名称<el-input v-model="input" :resize="horizontal"></el-input></div></el-col>
+  <el-col :span="6"><div class="grid-content bg-purple">  <el-button size="mini" round>生成台账</el-button></div></el-col>
+</el-row>
+        
+     
+        
       </div>
-      <dev class="grid-content bg-purple">
+      <div class="grid-content bg-purple">
         <el-table :data="tableData" border style="width: 100%">
           <el-table-column prop="date" label="日期" width="180">
           </el-table-column>
           <el-table-column prop="name" label="姓名" width="180">
           </el-table-column>
           <el-table-column prop="address" label="地址"> </el-table-column>
-        </el-table> </dev
+        </el-table></div
     ></el-col>
   </el-row>
 </template>
@@ -42,15 +70,41 @@
 export default {
   data() {
     return {
+      formInline: {
+        user: "",
+        region: "",
+      },
       data: [
         {
-          label: "一级 1",
+          label: "常规台账",
           children: [
             {
-              label: "二级 1-1",
+              label: "中东首府",
               children: [
                 {
-                  label: "三级 1-1-1",
+                  cid: 1,
+                  label: "水电费",
+                },
+              ],
+            },
+            {
+              label: "海边别墅",
+              children: [
+                {
+                  label: "煤气费",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: "公摊台账",
+          children: [
+            {
+              label: "中东首府",
+              children: [
+                {
+                  label: "卫生费",
                 },
               ],
             },
@@ -66,8 +120,11 @@ export default {
       },
     };
   },
-
-  components: {},
+  methods: {
+    handleNodeClick(data, node, component) {
+      // this.axios.get('http://localhost:8080/',data.cid)
+    },
+  },
 };
 </script>
 <style  scoped>
